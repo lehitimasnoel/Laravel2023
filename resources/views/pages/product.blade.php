d1a3002379fa88838ad21de83f83ee94a11ac8aa
lehitimasnoel <31946831+lehitimasnoel@users.noreply.github.com>
2023-10-12 22:35:51 +0800

    Add Products page

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 
-<div class="row row-cols-1 row-cols-md-3 g-4">
+@if($products->count() > 0)
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+@endif
 
-    @foreach ($products as $product)
+    @forelse ($products as $product)
+
         <div class="col">
             <div class="card h-100">
             <img src="{{ $product->image }}" style="height: 150px" class="card-img-top" alt="...">
@@ -17,7 +20,23 @@
             </div>
             </div>
         </div>
-    @endforeach
+
+        @empty
+        <div class="card text-center">
+            <div class="card-header">
+              Featured
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special Products Available</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Add Products</a>
+            </div>
+            <div class="card-footer text-muted">
+              2 days ago
+            </div>
+          </div>
+
+    @endforelse
 </div>
 
 {!! $products->onEachSide(6)->links() !!}
